03e714babb794ad5ceec9cfe229a2beb37f0823d
s sreesa <139852839+ssreesa12@users.noreply.github.com>
2024-02-20 10:55:08 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n[5]={1,5,7,9,4};
+    int k;
+    cout<<"enter an number to be searched";
+    cin>>k;
+    for(int i=0;i<5;i++){
+        if(n[i]==k){
+            cout<<"the number is present in index "<<i;
+        } 
+    }
+    return 0;
+}
+    
+
